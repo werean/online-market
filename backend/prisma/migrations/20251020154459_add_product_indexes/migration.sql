1504e83e15a1865546338aa2389072ba23ce52f7
Lucas Assis <lucasassis0507@gmail.com>
2025-10-20 16:06:00 -0300

    feat: add product indexes and update database schema

---
+++ backend/prisma/migrations/20251020154459_add_product_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Product_sellerId_idx" ON "Product"("sellerId");
+
+-- CreateIndex
+CREATE INDEX "Product_createdAt_idx" ON "Product"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Product_price_idx" ON "Product"("price");
